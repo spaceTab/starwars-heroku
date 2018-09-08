2bb76e6c2a0bb7e9ec0025bb3111f1daa42169b1
spaceTab <93plaistj@protonmail.ch>
2018-09-08 15:09:09 -0400

    server.js

--- 14-FinalStarwarsApp/server.js
+++ 14-FinalStarwarsApp/server.js
@@ -1,6 +1,5 @@
 // Dependencies
 // =============================================================
-require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
